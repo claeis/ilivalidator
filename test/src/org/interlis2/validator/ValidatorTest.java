5c949085ad073e9326048bd37130bd146d1f1bb0
claeis <claeis@users.noreply.github.com>
2016-09-09 10:56:39 +0200

    added tests to test general use and show high level API usage

---
+++ test/src/org/interlis2/validator/ValidatorTest.java
@@ -0,0 +1,69 @@
+package org.interlis2.validator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ch.ehi.basics.logging.EhiLogger;
+import ch.ehi.basics.settings.Settings;
+
+
+public class ValidatorTest {
+
+	@Test
+	public void itfOk() {
+		boolean ret=Validator.runValidation("test/data/Beispiel1a.itf", null);
+		assertTrue(ret);
+	}
+	@Test
+	public void itfOkWithSettings() {
+		Settings settings=new Settings();
+		settings.setValue(Validator.SETTING_ILIDIRS, Validator.ITF_DIR);
+		boolean ret=Validator.runValidation("test/data/Beispiel1a.itf", settings);
+		assertTrue(ret);
+	}
+	@Test
+	public void itfFail() {
+		boolean ret=Validator.runValidation("test/data/Beispiel1b.itf", null);
+		assertFalse(ret);
+	}
+	@Test
+	public void itfFailWithSettings() {
+		Settings settings=new Settings();
+		settings.setValue(Validator.SETTING_ILIDIRS, Validator.ITF_DIR);
+		boolean ret=Validator.runValidation("test/data/Beispiel1b.itf", settings);
+		assertFalse(ret);
+	}
+	@Test
+	public void xtfOk() {
+		boolean ret=Validator.runValidation("test/data/Beispiel2a.xtf", null);
+		assertTrue(ret);
+	}
+	@Test
+	public void xtfOkWithSettings() {
+		Settings settings=new Settings();
+		settings.setValue(Validator.SETTING_ILIDIRS, Validator.ITF_DIR);
+		boolean ret=Validator.runValidation("test/data/Beispiel2a.xtf", settings);
+		assertTrue(ret);
+	}
+	@Test
+	public void xtfFail() {
+		boolean ret=Validator.runValidation("test/data/Beispiel2b.xtf", null);
+		assertFalse(ret);
+	}
+	@Test
+	public void xtfFailWithSettings() {
+		Settings settings=new Settings();
+		settings.setValue(Validator.SETTING_ILIDIRS, Validator.ITF_DIR);
+		boolean ret=Validator.runValidation("test/data/Beispiel2b.xtf", settings);
+		assertFalse(ret);
+	}
+	@Test
+	public void ili2cFail() {
+		Settings settings=new Settings();
+		settings.setValue(Validator.SETTING_ILIDIRS, "emptyFolder"); // ili2c will not find any ili files there
+		boolean ret=Validator.runValidation("test/data/Beispiel2a.xtf", settings);
+		assertFalse(ret);
+	}
+
+}
