1af0b1291679c2850e99c5a9be49ba0bcbe1ce17
claeis <claeis@users.noreply.github.com>
2017-10-26 17:00:20 +0200

    missing class

---
+++ src/org/interlis2/validator/impl/ErrorTracker.java
@@ -0,0 +1,12 @@
+package org.interlis2.validator.impl;
+
+import ch.ehi.basics.logging.AbstractStdListener;
+
+public class ErrorTracker extends AbstractStdListener{
+
+	@Override
+	public void outputMsgLine(int arg0, int arg1, String arg2) {
+		// no output at all
+	}
+
+}
