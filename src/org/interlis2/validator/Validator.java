a2b980ee540fb81faf38210f552430aa366640c9
claeis <ce@eisenhutinformatik.ch>
2020-08-24 15:20:21 +0200

    fix type

--- src/org/interlis2/validator/Validator.java
+++ src/org/interlis2/validator/Validator.java
@@ -353,7 +353,9 @@ public class Validator {
 			ioxReader=new ItfReader(new java.io.File(filename));
 		}
 		if(ioxReader instanceof ItfReader2){
-			((ItfReader2) ioxReader).setIgnorePolygonBuildingErrors(skipGeometryErrors);
+		    if(skipGeometryErrors) {
+	            ((ItfReader2) ioxReader).setIgnorePolygonBuildingErrors(ItfReader2.POLYGON_BUILDING_ERRORS_OFF);
+		    }
             ((ItfReader2) ioxReader).setAllowItfAreaHoles(allowItfAreaHoles);
 		    ((ItfReader2) ioxReader).setIoxDataPool(pool);
 		}
